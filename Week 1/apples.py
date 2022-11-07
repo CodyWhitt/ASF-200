4c8ad6e2040dbd4743958d43ef8f629c14e09e62
CodyWhitt <95008570+CodyWhitt@users.noreply.github.com>
2022-11-06 19:21:27 -0600

    initial commit

---
+++ Week 1/apples.py
@@ -0,0 +1,5 @@
+applePrice = .50
+name = str(input("Enter username: "))
+print('Hey', name , ', the price of the apple is $', applePrice)
+amntPrch = int(input("How many would you like to buy? "))
+print('Thanks', name ,'for your purchase of', amntPrch, 'apples @ $', applePrice, '.ea')
