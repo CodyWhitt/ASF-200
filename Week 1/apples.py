b0ae809abed000151e326e2664ac412540574ac7
CodyWhitt <95008570+CodyWhitt@users.noreply.github.com>
2022-11-13 14:47:19 -0600

    resubmission

--- Week 1/apples.py
+++ Week 1/apples.py
@@ -1,5 +1,4 @@
-applePrice = .50
-name = str(input("Enter username: "))
-print('Hey', name , ', the price of the apple is $', applePrice)
-amntPrch = int(input("How many would you like to buy? "))
-print('Thanks', name ,'for your purchase of', amntPrch, 'apples @ $', applePrice, '.ea')
+applePrice = float(.50)
+name = str(input("Please enter your name: "))
+amt= int(input('Hi {}, Apples cost ${:.2f} each. How many would you like to buy?'.format(name,applePrice)))
+print('Thanks {} for your purchase of {} apples at a cost of ${:.2f} each'.format(name, amt, applePrice))
